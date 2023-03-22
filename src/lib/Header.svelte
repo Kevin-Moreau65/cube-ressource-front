ecdf59e2cb255e012d60957c8e1c8e2de64a7d6f
Kevin-Moreau65 <kevin.moreau655@gmail.com>
2023-03-22 17:15:48 +0100

    Added back button

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,9 +1,30 @@
-<script>
+<script lang="ts">
+	import { browser } from '$app/environment';
+	import { goto } from '$app/navigation';
 	import { storeTitle } from './store';
+	const gotoHome = async (e: KeyboardEvent) => {
+		if (e.key === 'Enter') {
+			await goto('/');
+		}
+	};
 </script>
 
 <header class="main">
+	<img
+		src="/back-arrow.svg"
+		class="back"
+		alt="Bouton de retour"
+		on:click={() => history.back()}
+		on:keydown={(e) => e.key === 'Enter' && history.back()}
+	/>
 	<h1>{$storeTitle}</h1>
+	<img
+		src="/Re.svg"
+		class="icon"
+		alt="Icône de l'application"
+		on:click={async () => await goto('/')}
+		on:keydown={(e) => gotoHome(e)}
+	/>
 </header>
 
 <style>
@@ -17,5 +38,22 @@
 		color: white;
 		text-align: center;
 		margin: 0;
+	}
+	.icon {
+		top: -10px;
+		right: -10px;
+		position: fixed;
+		height: 70px;
+		width: 70px;
+		cursor: pointer;
+	}
+	.back {
+		transform: rotate(180deg);
+		position: fixed;
+		top: 0;
+		left: 10px;
+		height: 50px;
+		width: 40px;
+		cursor: pointer;
 	}
 </style>
